84d132c392dcc2a380c04c878f91b9fec8ff7c54
Hiteshmeher <meher.hitesh4@gmail.com>
2022-10-12 19:22:58 +0530

    [Hitesh]: Created Rest Controller with put method and path variable

--- RestAPI/src/main/java/com/bridgelabz/restapi/restapi/controller/HelloController.java
+++ RestAPI/src/main/java/com/bridgelabz/restapi/restapi/controller/HelloController.java
@@ -24,8 +24,14 @@ public class HelloController {
     }
 
     //get request mapping with request body
-    @PostMapping("/hello")
+    @PostMapping("/hello/post")
     public String getFirstLastName(@RequestBody Person person){
         return "Hello "+ person.getFirstName() +" "+ person.getLastName() +" from Bridgelabz";
+    }
+
+    //put request mapping with path variable and query parameter
+    @PutMapping("/hello/put/{firstName}")
+    public String sayHelloWithPut(@RequestParam String lastName, @PathVariable String firstName){
+        return "Hello "+ firstName +" "+ lastName +" from Bridgelabz";
     }
 }
